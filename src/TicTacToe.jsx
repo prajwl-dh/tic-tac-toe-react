9b14b4e3a9d015de1d340ea26853b703ba3aaecf
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-04-25 16:02:06 -0500

    Refactor TicTacToe component and update index.css

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -1,3 +1,12 @@
 export default function TicTacToe() {
-  return <div className='text text-red-500'>Test</div>;
+  return (
+    <div className='flex justify-center items-center h-screen w-screen'>
+      <p className='text-[3rem] md:text-[6rem] font-bold leading-none'>
+        Tic Tac Toe
+      </p>
+      <div className='bg-slate-200 backdrop-blur-sm bg-opacity-40 rounded-lg shadow-sm'>
+        hi
+      </div>
+    </div>
+  );
 }
