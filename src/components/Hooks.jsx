8dc4a5eb306b4eaf000f3893d6b58eef9a9ac923
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-04-29 10:55:01 -0500

    Add react-confetti and react-use as dependencies, and update Hooks component to handle winner and tie conditions

--- src/components/Hooks.jsx
+++ src/components/Hooks.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const Hooks = () => {
   const [board, setBoard] = React.useState(Array(9).fill(null));
   const [xTurn, setXTurn] = React.useState(true);
+  const [winner, setWinner] = React.useState(null);
+  const [tie, setTie] = React.useState(null);
 
   const handleClick = (index) => {
     if (xTurn) {
@@ -10,15 +12,47 @@ const Hooks = () => {
     } else {
       board[index] = 'O';
     }
+    setWinner(checkWinner());
+    setTie(tieChecker());
     setXTurn(!xTurn);
   };
 
   const resetGame = () => {
+    setTie(null);
+    setWinner(null);
     setBoard(Array(9).fill(null));
     setXTurn(true);
   };
 
-  return { board, xTurn, handleClick, resetGame };
+  const checkWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+        return board[a];
+      }
+    }
+  };
+
+  const tieChecker = () => {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === null) {
+        return null;
+      }
+    }
+    return 'Tie';
+  };
+
+  return { board, xTurn, handleClick, resetGame, winner, tie };
 };
 
 export default Hooks;
--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,7 +1,10 @@
+import Confetti from 'react-confetti';
+import useWindowSize from 'react-use/lib/useWindowSize';
 import Hooks from './Hooks';
 
 export default function TicTacToe() {
-  const { board, xTurn, handleClick, resetGame } = Hooks();
+  const { board, xTurn, handleClick, resetGame, winner, tie } = Hooks();
+  const { width, height } = useWindowSize();
 
   return (
     <div className='flex flex-col gap-10 md:gap-5 justify-center items-center h-screen w-screen'>
@@ -10,25 +13,46 @@ export default function TicTacToe() {
       </p>
       <div className='bg-slate-200 flex flex-col backdrop-blur-sm bg-opacity-40 rounded-lg shadow-sm w-[96vw] md:w-[60vw]'>
         <div className='flex flex-row justify-between items-end p-2 md:p-5'>
-          <p className='text text-gray-700 font-bold text-3xl'>
-            Turn : <span className='text font-normal'>{xTurn ? 'X' : 'O'}</span>
-          </p>
+          {winner === 'X' || winner === 'O' ? (
+            <p className='text text-gray-700 font-bold text-3xl'>
+              Winner : <span className='text font-normal'>{winner}</span>
+            </p>
+          ) : tie?.length === 0 ? (
+            <p className='text text-gray-700 font-bold text-3xl'>
+              Turn :{' '}
+              <span className='text font-normal'>{xTurn ? 'X' : 'O'}</span>
+            </p>
+          ) : (
+            <p className='text text-gray-700 font-bold text-3xl'>
+              Winner : <span className='text font-normal'>{tie}</span>
+            </p>
+          )}
           <button
             onClick={() => resetGame()}
             className='px-2 py-1 rounded-md bg-red-600 text-white hover:bg-red-500 hover:scale-105 active:bg-red-500 transition duration-300 text-xl'
           >
-            Reset Game
+            {winner == 'X' || winner == 'O' || tie?.length > 0
+              ? 'Play Again'
+              : 'Reset'}
           </button>
         </div>
         <div className='grid grid-cols-3 m-2 md:m-5 border-2 border-gray-500 rounded-sm'>
           {board.map((_, index) => {
             return (
               <button
-                className='border-2 border-gray-500 h-24 md:h-36 text-5xl text-gray-700 overflow-hidden'
+                className={`border-2 border-gray-500 h-24 md:h-36 text-5xl text-gray-700 overflow-hidden ${
+                  board[index] === 'X' || board[index] === 'O' ? 'bg-white' : ''
+                }`}
                 key={index}
                 onClick={() => handleClick(index)}
                 disabled={
-                  board[index] === 'X' || board[index] === 'O' ? true : false
+                  winner === 'X' ||
+                  winner === 'O' ||
+                  tie?.length > 0 ||
+                  board[index] === 'X' ||
+                  board[index] === 'O'
+                    ? true
+                    : false
                 }
               >
                 {board[index]}
@@ -37,6 +61,9 @@ export default function TicTacToe() {
           })}
         </div>
       </div>
+      {winner == 'X' || winner == 'O' || tie?.length > 0 ? (
+        <Confetti width={width} height={height} />
+      ) : null}
     </div>
   );
 }
